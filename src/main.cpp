bf6e48712fc4734dedc912e36136af563ae67d96
Winfried Auner <18383180+wAuner@users.noreply.github.com>
2024-10-14 17:10:11 +0200

    test

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,5 @@ int main()
     // default namespace if not changed via set_target_properties Swift_MODULE_NAME
     //Interop::sayHello();
     std::println("Hello from C++");
-    return 0;
+    return 0; 
 }
